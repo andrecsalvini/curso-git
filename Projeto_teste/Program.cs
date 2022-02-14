b5d610b1a94917902b63b6f54a71ad5b797308c8
Andre Cassarro Salvini <andrecassarros@gmail.com>
2022-02-14 12:39:42 -0300

    Acrescentada uma mensagem desejadno um bom dia!

--- Projeto_teste/Program.cs
+++ Projeto_teste/Program.cs
@@ -7,11 +7,11 @@ namespace Projeto_teste
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Olá mundo!");
-			Console.WriteLine("Bom dia!");
 			Console.WriteLine("Como você está?");
+			Console.WriteLine("Tenha um bom dia!");
 
 			Console.ReadLine();
-
+		
 		}
 	}
 }
